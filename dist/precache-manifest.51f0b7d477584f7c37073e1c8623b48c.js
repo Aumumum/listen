c1322416b8eadb64807bae2d0b749b2de5e7527e
Auyeungme <202193025@qq.com>
2021-09-21 21:52:20 +0800

    fan

---
+++ dist/precache-manifest.51f0b7d477584f7c37073e1c8623b48c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae38b11ecb2cddec5df",
+    "url": "/css/chunk-vendors.7646b3be.css"
+  },
+  {
+    "revision": "4043af09d79a0ed1b9b7",
+    "url": "/css/index.25de1df0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "86bcb0b3ee0e01de71ffdea15b542240",
+    "url": "/img/cddisk.86bcb0b3.png"
+  },
+  {
+    "revision": "2634051a7fdc688a959679722b5a0bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae38b11ecb2cddec5df",
+    "url": "/js/chunk-vendors.d180f1f9.js"
+  },
+  {
+    "revision": "4043af09d79a0ed1b9b7",
+    "url": "/js/index.c64826eb.js"
+  },
+  {
+    "revision": "b9d79419c8051857dbc1dedbc00e2ab2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";import animate from "animate.css";
 import "./static/normalize.css";
 
 import "./plugins/element";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
